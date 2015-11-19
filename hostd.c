ab199b794cc1f5da44293663bcd80f1abf22f732
khirthana <khirthana.subramanian@uoit.net>
2015-11-19 18:46:53 -0500

    updated hostd.c with 4 process queues

--- hostd.c
+++ hostd.c
@@ -41,40 +41,136 @@ int main(int argc, char *argv[]) {
   FILE fp = argv [1];
 
   // Load the dispatch list from the file
+  // Add each process structure instance to the job dispatch list queue
   load_dispatch(fp, queue);
 
 
-      // Add each process structure instance to the job dispatch list queue
-
-    // Iterate through each item in the job dispatch list, add each process
-    // to the appropriate queues
+  // Iterate through each item in the job dispatch list, add each process
+  // to the appropriate queues
+  //Execute the ​process​ binary using fork and exec
   node_t *current = queue;
+
   while (current != NULL) {
+
     if(current­>process.priority==0){
       push(real_time_queue,process);
+
+          pid_t pid;
+
+//execute process until their runtime is completed
+
+
+           if (pid == 0){
+           // set function calls
+           signal(SIGINT, SIG_DFL);
+           signal(SIGCONT, SIG_DFL);
+           signal(SIGTSTP, SIG_DFL);
+           execvp (args[0], args);
+           }
+           else{
+            sleep(5);
+            kill(pid,SIGINT);
+            sleep(10);
+            kill(pid,SIGCONT);
+            sleep(10);
+            printf("\nPARENT: sending SIGTSTP\n\n");
+            kill(pid,SIGTSTP);
+            waitpid(pid, 0, 0);   // wait for child to exit
+          }
     }
     else if(current­>process.priority==1){
+
       push(priority_1_queue,process);
+            pid_t pid;
+            pop(priority_1_queue);
+
+//execute process for 1 sec
+
+
+              //after a process has been run for ​1 second​ it is removed from the queue and added to the next lower priority queue
+              push(priority_2_queue,process);
+
+              if (pid == 0){
+              // set function calls
+              signal(SIGINT, SIG_DFL);
+              signal(SIGCONT, SIG_DFL);
+              signal(SIGTSTP, SIG_DFL);
+              execvp (args[0], args);
+              }
+              else{
+               sleep(5);
+               kill(pid,SIGINT);
+               sleep(10);
+               kill(pid,SIGCONT);
+               sleep(10);
+               printf("\nPARENT: sending SIGTSTP\n\n");
+               kill(pid,SIGTSTP);
+               waitpid(pid, 0, 0);   // wait for child to exit
+             }
     }
     else if(current­>process.priority==2){
+
       push(priority_2_queue,process);
+          pid_t pid;
+          pop(priority_2_queue);
+
+//execute process for 1 sec
+
+
+          //after a process has been run for ​1 second​ it is removed from the queue and added to the next lower priority queue
+          push(priority_3_queue,process);
+
+          if (pid == 0){
+          // set function calls
+          signal(SIGINT, SIG_DFL);
+          signal(SIGCONT, SIG_DFL);
+          signal(SIGTSTP, SIG_DFL);
+          execvp (args[0], args);
+          }
+          else{
+           sleep(5);
+           kill(pid,SIGINT);
+           sleep(10);
+           kill(pid,SIGCONT);
+           sleep(10);
+           printf("\nPARENT: sending SIGTSTP\n\n");
+           kill(pid,SIGTSTP);
+           waitpid(pid, 0, 0);   // wait for child to exit
+         }
     }
     else{
+
       push(priority_3_queue,process);
+          pid_t pid;
+          pop(priority_3_queue);
+
+//execute process for 1 sec
+
+
+          //after a process has been run for ​1 second​ it is removed from the queue and added back to priority 3 queue
+          push(priority_3_queue,process);
+
+          if (pid == 0){
+          // set function calls
+          signal(SIGINT, SIG_DFL);
+          signal(SIGCONT, SIG_DFL);
+          signal(SIGTSTP, SIG_DFL);
+          execvp (args[0], args);
+          }
+          else{
+           sleep(5);
+           kill(pid,SIGINT);
+           sleep(10);
+           kill(pid,SIGCONT);
+           sleep(10);
+           printf("\nPARENT: sending SIGTSTP\n\n");
+           kill(pid,SIGTSTP);
+           waitpid(pid, 0, 0);   // wait for child to exit
+         }
     }
     current = current­>next;
   }
 
-
-
-    // Allocate the resources for each process before it's executed
-
-    // Execute the process binary using fork and exec
-
-
-    // Perform the appropriate signal handling / resource allocation and de-alloaction
-
-    // Repeat until all processes have been executed, all queues are empty
 
     return EXIT_SUCCESS;
 }
