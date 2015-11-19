511a7bdc81bc3a126ce5ab67112812ec3a6a76b9
khirthana <khirthana.subramanian@uoit.net>
2015-11-19 18:21:13 -0500

    added 4 process queues

--- hostd.c
+++ hostd.c
@@ -21,6 +21,14 @@
 #define MEMORY 1024
 
 // Put global environment variables here
+node_t *queue;
+proc process;
+
+node_t *real_time_queue;
+node_t *priority_1_queue;
+node_t *priority_2_queue;
+node_t *priority_3_queue;
+
 
 // Define functions declared in hostd.h here
 
@@ -32,20 +40,37 @@ int main(int argc, char *argv[]) {
 
   FILE fp = argv [1];
 
+  // Load the dispatch list from the file
   load_dispatch(fp, queue);
 
-    // ==================== YOUR CODE HERE ==================== //
 
-    // Load the dispatch list from the file
-
-    // Add each process structure instance to the job dispatch list queue
+      // Add each process structure instance to the job dispatch list queue
 
     // Iterate through each item in the job dispatch list, add each process
     // to the appropriate queues
+  node_t *current = queue;
+  while (current != NULL) {
+    if(current­>process.priority==0){
+      push(real_time_queue,process);
+    }
+    else if(current­>process.priority==1){
+      push(priority_1_queue,process);
+    }
+    else if(current­>process.priority==2){
+      push(priority_2_queue,process);
+    }
+    else{
+      push(priority_3_queue,process);
+    }
+    current = current­>next;
+  }
+
+
 
     // Allocate the resources for each process before it's executed
 
     // Execute the process binary using fork and exec
+
 
     // Perform the appropriate signal handling / resource allocation and de-alloaction
 
